7e5cf4606518014f0d00f021438378eb463e8488
raenion <nkoudeih@gmail.com>
2025-12-20 15:05:26 +0000

    Update 1Danim.py

--- Heat Equation/1D/Periodic/1Danim.py
+++ Heat Equation/1D/Periodic/1Danim.py
@@ -1,42 +1,63 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+"""
+1D Heat Equation
+Periodic boundary conditions
+Explicit Euler scheme
+"""
+
+# Space:
+
 nx = 100
+dx = 2/(nx-1)
+x = np.linspace(-1, 1, nx)
+
+# Time:
+
 dt = 0.0001
 T = 1
-dx = 2/(nx-1)
-nu = 0.1
-
 N = int(T/dt)+1
 
-x = np.linspace(-1,1,nx)
+# Physics:
+
+nu = 0.1
+
+# Stability check and conditional adjust:
+
+if dt > dx**2/(2*nu):
+    dt = dx**2/(2*nu)
+
+# Initialization:
+
 #u0 = np.cos(2*np.pi*x) + 1/2*np.cos(4*np.pi*x)
 u0 = np.sin(4*np.pi*x) + 1/3*np.sin(7*np.pi*x)
 
 u = np.zeros((N,nx))
-t = np.zeros(N)
 
 u[0] = u0
-t[0] = 0
 
 
 def laplacian(f):
+
+    # Periodic boundary conditions via np.roll:
+    
     return (np.roll(f,-1) - 2*f + np.roll(f, 1))/dx**2
+
+# Explicit Euler scheme integrator loop:
 
 for i in range(N-1):
     
     u[i+1] = u[i] + nu*laplacian(u[i])*dt
-    
-    t[i+1] = t[i] + dt
 
 
-## Animate 
+# Animation: 
 
 from matplotlib.animation import FuncAnimation
 
 fig, ax = plt.subplots()
 
-step = 10 # gap between frames is this many integrator time steps
+step = 10 # Gap between frames is this many integrator time steps.
 
 umin = np.min(u)
 umax = np.max(u)
@@ -46,13 +67,12 @@ ax.set_xlim(-1, 1)
 
 line, = ax.plot([], [])
 
-
 def update(frames):
     line.set_data(x, u[frames])
 
-    return line,                  ## 
+    return line,
 
-frames = range(0, len(u), step) # frames can be an iterable as well as int 
+frames = range(0, len(u), step) # frames can be an iterable as well as int.
 
 ani = FuncAnimation(fig, update, frames=frames, interval=20)
 
